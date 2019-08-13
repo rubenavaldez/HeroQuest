ed2856dcb80ecd6349664b21ebd1ce83b91725ff
Ruben Valdez <ruben.valdez7@gmail.com>
2019-08-12 20:23:26 -0700

    add campaign screen and game landing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,12 +8,6 @@ module.exports = function(app) {
     });
   });
 
-  app.get("/api/party", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
   // Create a new example
   app.post("/api/examples", function(req, res) {
     db.Example.create(req.body).then(function(dbExample) {
