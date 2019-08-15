7fd378ecbd5b4812e8ab31001d915b9b94708207
Ruben Valdez <ruben.valdez7@gmail.com>
2019-08-14 21:31:26 -0700

    updated model query

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,19 +3,19 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Characters.findAll({}).then(function(dbCharacters) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        Characters: dbCharacters
       });
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+  // Load Characters page and pass in an Characters by id
+  app.get("/Characters/:id", function(req, res) {
+    db.Characters.findOne({ where: { id: req.params.id } }).then(function(dbCharacters) {
+      res.render("Characters", {
+        Characters: dbCharacters
       });
     });
   });
@@ -23,10 +23,10 @@ module.exports = function(app) {
   // Load charcter select
   app.get("/party/:id", function(req, res) {
     console.log("party id");
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Characters.findAll({}).then(function(dbCharacters) {
       res.render("party", {
         msg: "Choose your party",
-        examples: dbExamples
+        Characters: dbCharacters
       });
     });
   });
