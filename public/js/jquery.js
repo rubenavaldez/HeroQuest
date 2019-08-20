8e56a449449d3ae52274e9bcee53dbb896dfab45
Ruben Valdez <ruben.valdez7@gmail.com>
2019-08-19 19:17:03 -0700

    jquery merge

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-unused-vars */
+var interval;
+$(".death").hide();
+$(".heal").hide();
 $(document).ready(function() {
   console.log("jQuery");
   $(".attack").hide();
   $(".death").hide();
+  $(".heal").hide();
 });
 
 var health = parseInt($(".active", this).attr("data-health"));
@@ -11,3 +15,42 @@ console.log(health);
 // console.log($(".active").attr("strength"));
 
 console.log($(".player3-idle", this).attr("health"));
+$(document).on("click", "#action-btn", function() {
+  console.log($(this).attr("value"));
+  clearInterval(interval);
+
+  if ($(this).attr("value") === "attack") {
+    console.log("attacking");
+    $(".idle").hide();
+    $(".attack").show();
+    setTimeout(attackAnimation, 1000);
+  } else if ($(this).attr("value") === "recover") {
+    console.log("recovering");
+    $(".heal").show();
+    setTimeout(healAnimation, 2000);
+  }
+});
+
+function attackAnimation() {
+  $(".idle").show();
+  $(".attack").hide();
+}
+
+$(document).on("click", "#start-game", function() {
+  console.log($(this).attr("value"));
+});
+
+function healAnimation() {
+  $(".heal").hide();
+}
+
+function enemyAttack(){
+  $(".enemy-idle").hide();
+  $(".enemy-attack").show();
+  setTimeout(enemyAttackAnimation, 2000);
+}
+
+function enemyAttackAnimation (){
+  $(".enemy-idle").show();
+  $(".enemy-attack").hide();
+}
