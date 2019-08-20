33688e4234e7ebfdb6877cfdb33f93a07b62c639
Ruben Valdez <ruben.valdez7@gmail.com>
2019-08-19 21:29:58 -0700

    enemy on screen

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -38,6 +38,56 @@ function attackAnimation() {
 
 $(document).on("click", "#start-game", function() {
   console.log($(this).attr("value"));
+  enemySelect = 1;
+  queryURL = "/api/enemies/" + enemySelect;
+  console.log(queryURL)
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(enemy) {
+
+    attackingDiv = $(".enemy-forward");
+    attackAnimationDiv = $("<div>");
+    attackAnimationDiv.attr(
+      "class",
+      "enemy" + enemySelect + "-idle enemy-attack enemy"
+    );
+    attackAnimationDiv.attr("src", enemy.image_attack);
+    attackAnimationDiv.attr("style", "min-height:300.6px;");
+    console.log(attackAnimationDiv);
+    attackingDiv.append(attackAnimationDiv);
+
+    enemyVisibleDiv = $(".enemy-visible-div");
+    enemyIdleDiv = $("<div>");
+    enemyIdleDiv.attr("class", "enemy-idle");
+    enemyIdleImage = $("img");
+    enemyIdleImage.attr(
+      "class",
+      "enemy" + enemySelect + "-idle idle enemy active"
+    );
+    enemyIdleImage.attr("data-health", enemy.health);
+    enemyHealth = enemy.health;
+    enemyIdleImage.attr("data-strength", enemy.strength);
+    enemyIdleImage.attr("data-defense", enemy.defense);
+    enemyIdleImage.attr("src", enemy.death);
+    enemyIdleImage.attr("style", "min-height:300.6px");
+    enemyIdleDiv.append(enemyIdleImage);
+
+
+    enemyDeathDiv = $("<div>");
+    enemyDeathImage = $("<img>");
+    enemyDeathImage.attr("class", "player" + enemySelect + "-death");
+    enemyDeathImage.attr("src", enemy.image_death);
+    enemyDeathImage.attr("style", "min-height:300.6px;");
+    enemyDeathDiv.append(enemyDeathImage);
+    enemyVisibleDiv.append(enemyIdleDiv);
+    enemyVisibleDiv.append(enemyDeathImage);
+    $("enemy-attack").hide();
+    $("enemy-death").hide();
+    $("enemy-idle").show();
+    console.log(enemy);
+  });
+  enemySelect++;
 });
 
 function healAnimation() {
