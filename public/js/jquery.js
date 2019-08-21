1d4e52a5b058231bab4731bd83857ebefcf65e25
Ruben Valdez <ruben.valdez7@gmail.com>
2019-08-21 01:26:17 -0700

    enemy animations

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -1,6 +1,13 @@
-$(window).on("load", function () {
+var enemySelect;
+var interval;
+var enemyHealth;
+var enemyStrength;
+var enemyDefense;
+var deathTime;
+
+$(window).on("load", function() {
   console.log($(this).attr("value"));
-  enemySelect = 0;
+  enemySelect = 2;
   queryURL = "/api/enemies/";
   console.log(queryURL);
   $.ajax({
@@ -35,10 +42,10 @@ $(window).on("load", function () {
     enemyIdleImage.attr(
       "class",
       "enemy" +
-      enemySelect +
-      "-idle idle enemy active enemy-idle" +
-      enemy[enemySelect].name +
-      "_idle"
+        enemySelect +
+        "-idle idle enemy active enemy-idle" +
+        enemy[enemySelect].name +
+        "_idle"
     );
     enemyIdleImage.attr("src", enemy[enemySelect].image_idle);
     enemyIdleDiv.append(enemyIdleImage);
@@ -50,15 +57,75 @@ $(window).on("load", function () {
     enemyDeathImage.attr(
       "class",
       "player" +
-      enemySelect +
-      "-death death enemy active" +
-      enemy[enemySelect].name +
-      "_death"
+        enemySelect +
+        "-death death enemy active" +
+        enemy[enemySelect].name +
+        "_death"
     );
     enemyDeathImage.attr("src", enemy[enemySelect].image_death);
     enemyDeathImage.attr("style", "min-height:300.6px;");
 
     enemyDeathDiv.append(enemyDeathImage);
     enemyVisibleDiv.append(enemyDeathDiv);
+    $(".player-attack").hide();
+    $(".player-death").hide();
+    $(".heal").hide();
+    $(".enemy-attack").hide();
+    $(".enemy-death").hide();
+    enemyDefense = enemy[enemySelect].defense;
+    enemyHealth = enemy[enemySelect].health;
+    enemyStrength = enemy[enemySelect].strength;
+  });
+  $(document).on("click", "#action-btn", function () {
+    console.log($(this).attr("value"));
+    clearInterval(interval);
+
+    if ($(this).attr("value") === "attack") {
+      console.log("attacking");
+      $(".player-idle").hide();
+      $(".player-attack").show();
+      setTimeout(attackAnimation, 1000);
+    } else if ($(this).attr("value") === "recover") {
+      console.log("recovering");
+      $(".heal").show();
+      setTimeout(healAnimation, 2000);
+    }
   });
 });
+
+function attackAnimation() {
+  $(".player-idle").show();
+  $(".player-attack").hide();
+}
+function healAnimation() {
+  $(".heal").hide();
+}
+
+function enemyAttack() {
+  $(".enemy-idle").hide();
+  $(".enemy-attack").show();
+  setTimeout(enemyAttackAnimation, 2000);
+}
+
+function enemyAttackAnimation() {
+  $(".enemy-idle").show();
+  $(".enemy-attack").hide();
+}
+
+function enemyDeath(){
+  $(".enemy-death").show();
+  $(".enemy-idle").hide();
+
+  setTimeout(enemyDeathAnimation, 1200);
+}
+
+function enemyDeathAnimation() {
+  $(".enemy-idle").hide();
+  $(".enemy-death").hide();
+};
+
+function deathTimer(){
+
+}
+
+deathTimer(enemySelect)
